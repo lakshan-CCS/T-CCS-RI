479dc1188ad92534fc09b46a6386f9bbb7d32e64
arg0nw <arunawickram@gmail.com>
2017-03-19 00:25:05 +0530

    Revert "material design update"
    This reverts commit a279c4b5eae1df811c1b0c52b387edcab3386056.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-import { AppComponent }  from './app.component';
-import 'hammerjs';
 
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule,
-                  FormsModule,
-                  HttpModule,
-                  MaterialModule.forRoot() ],
+  imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
-  providers:[],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
