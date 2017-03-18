a279c4b5eae1df811c1b0c52b387edcab3386056
arg0nw <arunawickram@gmail.com>
2017-03-18 23:59:19 +0530

    material design update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 import { AppComponent }  from './app.component';
+import 'hammerjs';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,
+                  FormsModule,
+                  HttpModule,
+                  MaterialModule.forRoot() ],
   declarations: [ AppComponent ],
+  providers:[],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
